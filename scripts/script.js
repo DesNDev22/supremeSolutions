6180095cb95d5eef24efdb7736b219b940a77e0c
Luis Carrera <luiscarreradesndev@gmail.com>
2023-10-22 19:25:18 -0500

    mobile menu

--- scripts/script.js
+++ scripts/script.js
@@ -20,3 +20,31 @@ window.addEventListener('scroll', toggleCTAButton);
 
 // Initial check to show/hide the button on page load
 toggleCTAButton();
+
+// Mobile menu
+const menu = document.querySelector('.menu-items');
+const menuItem = document.querySelectorAll('.menuItem');
+const hamburguer = document.querySelector('.hamburguer');
+const closeIcon = document.querySelector('.close-icon');
+const menuIcon = document.querySelector('.menu-icon');
+const itemLink = document.querySelector('.item-link');
+
+function toggleMenu() {
+    if (menu.classList.contains('showMenu')) {
+        menu.classList.remove('showMenu')
+        closeIcon.style.display = "none"
+        menuIcon.style.display = "block"
+    } else {
+        menu.classList.add('showMenu')
+        closeIcon.style.display = "block"
+        menuIcon.style.display = "none"
+    }
+}
+
+hamburguer.addEventListener("click", toggleMenu);
+
+menuItem.forEach(
+    function(menuItems) {
+        menuItems.addEventListener("click", toggleMenu)
+    }
+)
