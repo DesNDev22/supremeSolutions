4fa71d4099cfea75c67e2ebef9e4d2fad2765306
Luis Carrera <luiscarreradesndev@gmail.com>
2023-10-26 16:51:02 -0500

    fixed floating menu on mobile

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,15 @@
+// Floating CTA
 const ctaButton = document.querySelector('.CTA-Float');
 const heroSection = document.querySelector('.hero');
 const footerSection = document.querySelector('.foot-page');
+// Mobile menu
+const menu = document.querySelector('.menu-items');
+const menuItem = document.querySelectorAll('.menuItem');
+const hamburguer = document.querySelector('.hamburguer');
+const closeIcon = document.querySelector('.close-icon');
+const menuIcon = document.querySelector('.menu-icon');
+const itemLink = document.querySelector('.item-link');
+
 
 // Function to check the scroll position and show/hide the CTA button
 function toggleCTAButton() {
@@ -13,6 +22,10 @@ function toggleCTAButton() {
   } else {
     ctaButton.style.bottom = '-100px'; // Hide the button
   }
+  //Remove the mobile menu if activated
+  if (menu.classList.contains('showMenu')) {
+    toggleMenu()
+  }
 }
 
 // Attach the scroll event listener
@@ -20,14 +33,6 @@ window.addEventListener('scroll', toggleCTAButton);
 
 // Initial check to show/hide the button on page load
 toggleCTAButton();
-
-// Mobile menu
-const menu = document.querySelector('.menu-items');
-const menuItem = document.querySelectorAll('.menuItem');
-const hamburguer = document.querySelector('.hamburguer');
-const closeIcon = document.querySelector('.close-icon');
-const menuIcon = document.querySelector('.menu-icon');
-const itemLink = document.querySelector('.item-link');
 
 function toggleMenu() {
     if (menu.classList.contains('showMenu')) {
