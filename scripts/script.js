5e5436a59e16556c026f7670fb1972bda318409f
Luis Carrera <luiscarreradesndev@gmail.com>
2023-10-28 18:14:25 -0500

    fixed menu issues when loading other pages and general formatting

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,8 @@
+window.addEventListener('load', initializeMenu);
 // Floating CTA
 const ctaButton = document.querySelector('.CTA-Float');
 const heroSection = document.querySelector('.hero');
 const footerSection = document.querySelector('.foot-page');
-// Mobile menu
-const menu = document.querySelector('.menu-items');
-const menuItem = document.querySelectorAll('.menuItem');
-const hamburguer = document.querySelector('.hamburguer');
-const closeIcon = document.querySelector('.close-icon');
-const menuIcon = document.querySelector('.menu-icon');
-const itemLink = document.querySelector('.item-link');
-
 
 // Function to check the scroll position and show/hide the CTA button
 function toggleCTAButton() {
@@ -22,10 +15,6 @@ function toggleCTAButton() {
   } else {
     ctaButton.style.bottom = '-100px'; // Hide the button
   }
-  //Remove the mobile menu if activated
-  if (menu.classList.contains('showMenu')) {
-    toggleMenu()
-  }
 }
 
 // Attach the scroll event listener
@@ -34,7 +23,15 @@ window.addEventListener('scroll', toggleCTAButton);
 // Initial check to show/hide the button on page load
 toggleCTAButton();
 
-function toggleMenu() {
+function initializeMenu () {
+  // Mobile menu
+  const menu = document.querySelector('.menu-items');
+  const menuItem = document.querySelectorAll('.menuItem');
+  const hamburguer = document.querySelector('.hamburguer');
+  const closeIcon = document.querySelector('.close-icon');
+  const menuIcon = document.querySelector('.menu-icon');
+  const itemLink = document.querySelector('.item-link');
+  function toggleMenu() {
     if (menu.classList.contains('showMenu')) {
         menu.classList.remove('showMenu')
         closeIcon.style.display = "none"
@@ -44,12 +41,13 @@ function toggleMenu() {
         closeIcon.style.display = "block"
         menuIcon.style.display = "none"
     }
-}
+  }
 
-hamburguer.addEventListener("click", toggleMenu);
+  hamburguer.addEventListener("click", toggleMenu);
 
-menuItem.forEach(
+  menuItem.forEach(
     function(menuItems) {
         menuItems.addEventListener("click", toggleMenu)
     }
-)
+  )
+}
