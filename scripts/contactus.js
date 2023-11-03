17fd74b6d497d425ba75a429d4bcded8739f12ab
Luis Carrera <luiscarreradesndev@gmail.com>
2023-11-03 11:06:39 -0500

    added google map to contactus screen

--- scripts/contactus.js
+++ scripts/contactus.js
@@ -1,1 +1,31 @@
-// Map key AIzaSyBi3djoAF0tnGEvLm7Yxbl0ChbYBB9ZCNE
+// Get the Google Maps API key from the environment variable
+var googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY;
+
+function initMap() {
+    // Initialize the Google Map using the API key and set the center and zoom level
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 29.80054117046171, lng: -95.41337116441802 }, // Replace with your actual latitude and longitude
+        zoom: 15
+    });
+
+    // Add a marker to the map
+    var marker = new google.maps.Marker({
+        position: { lat: 29.80054117046171, lng: -95.41337116441802 }, // Replace with your actual latitude and longitude
+        map: map,
+        title: 'Business Location'
+    });
+}
+
+// Load the Google Maps JavaScript API script with your API key
+function loadGoogleMapsScript() {
+    var script = document.createElement('script');
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=' + googleMapsApiKey + '&callback=initMap';
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+}
+
+// Call the function to load the Google Maps API
+window.addEventListener('load', loadGoogleMapsScript);
+
+
