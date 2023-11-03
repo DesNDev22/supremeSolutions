ba945cabb189fcd9270f933a0c0216978e0e0e83
Luis Carrera <luiscarreradesndev@gmail.com>
2023-11-03 14:02:08 -0500

    taking back to simplified form

--- functions/api-key.js
+++
@@ -1,9 +0,0 @@
-// Netlify Function to retrieve the API key
-exports default async (req, context) => {
-    // Securely fetch and return the API key
-    const apiKey = Netlify.env.get("GOOGLE_MAPS_API_KEY");
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ apiKey })
-    };
-};
--- scripts/contactus.js
+++ scripts/contactus.js
@@ -1,38 +1,18 @@
 // Display Google Maps with business location
 function initMap() {
-    // Initialize the Google Map using the API key and set the center and zoom level
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 29.80054117046171, lng: -95.41337116441802 }, // Replace with your actual latitude and longitude
+    const location = { lat: 29.80054117046171, lng: -95.41337116441802 }; // Replace with your actual latitude and longitude
+
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: location,
         zoom: 15
     });
 
-    // Add a marker to the map
-    var marker = new google.maps.Marker({
-        position: { lat: 29.80054117046171, lng: -95.41337116441802 }, // Replace with your actual latitude and longitude
+    const marker = new google.maps.Marker({
+        position: location,
         map: map,
-        title: 'Supreme Solutions'
+        title: 'Marker Title'
     });
 }
 
-// Load the Google Maps JavaScript API script with your API key
-function loadGoogleMapsScript() {
-    // Fetch the API key from the serverless function
-    fetch('/.netlify/functions/api-key')
-        .then(response => response.json())
-        .then(data => {
-            const apiKey = data.apiKey;
-            // Use the apiKey in your Google Maps code
-            const script = document.createElement('script');
-            script.src = 'https://maps.googleapis.com/maps/api/js?key=' + apiKey + '&callback=initMap';
-            script.async = true;
-            script.defer = true;
-            document.head.appendChild(script);
-        })
-        .catch(error => {
-            alert ('Failed to retrieve the API key:', error);
-        });
-
-}
-
 // Call the function to load the Google Maps API
-window.addEventListener('load', loadGoogleMapsScript);
+window.addEventListener('load', initMap);
