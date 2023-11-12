98633c0bd2a7e5869cf718baf609ac2062e58d9a
Luis Carrera <luiscarreradesndev@gmail.com>
2023-11-12 14:52:02 -0600

    moving all calls to google maps to maps.js

--- scripts/map.js
+++ scripts/map.js
@@ -1,17 +1,44 @@
-// window.addEventListener('load', initMap);
-
 // // Display Google Maps with business location
+// function initMap() {
+//     const location = { lat: 29.80054117046171, lng: -95.41337116441802 };
+
+//     const map = new google.maps.Map(document.getElementById('map'), {
+//         center: location,
+//         zoom: 15
+//     });
+
+//     const marker = new google.maps.Marker({
+//         position: location,
+//         map: map,
+//         title: 'Marker Title'
+//     });
+// }
+// contact-map.js
 function initMap() {
     const location = { lat: 29.80054117046171, lng: -95.41337116441802 };
-
+  
     const map = new google.maps.Map(document.getElementById('map'), {
-        center: location,
-        zoom: 15
+      center: location,
+      zoom: 15,
     });
-
-    const marker = new google.maps.Marker({
-        position: location,
-        map: map,
-        title: 'Marker Title'
-    });
-}
+  
+    new google.maps.Marker({ position: location, map: map });
+  }
+  
+  // Load the Google Maps API with the API key
+  function loadGoogleMapsScript() {
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBi3djoAF0tnGEvLm7Yxbl0ChbYBB9ZCNE&callback=initMap`;
+    script.defer = true;
+    script.async = true;
+    document.head.appendChild(script);
+  }
+  
+  // Ensure the Google Maps API is loaded before calling initMap
+  if (typeof google === 'object' && typeof google.maps === 'object') {
+    initMap();
+  } else {
+    // If the API is not yet loaded, load the script
+    window.addEventListener('load', loadGoogleMapsScript);
+  }
+  
